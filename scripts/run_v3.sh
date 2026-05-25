fec288a855a8644a2822b95db9c14b187b9dc438
Rayhan <rayhanhaqi@github.com>
2026-05-25 19:59:54 +0800

    Fix CRLF line endings in v3 and restart shell scripts.
    Add .gitattributes to keep *.sh as LF on checkout.

--- scripts/run_v3.sh
+++ scripts/run_v3.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")/.."
 
 SKIP_INSTALL=1 SKIP_DOWNLOAD=1 SKIP_PREPROCESS=1 SKIP_SETUP=1 SKIP_TESTS=1 \
 LABEL_MODE=dots BUILD_DOT_CACHE=1 \
-  bash scripts/run_phase1.sh resnet50 30 128 1e-4 1 299 4 dots_v3
+  bash scripts/run_phase1.sh resnet50 30 128 1e-4 1 299 0 dots_v3
